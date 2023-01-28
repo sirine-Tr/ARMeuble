bb62aa73c2d7a0f4d245ed0bf351e745f0833309
sirine-Tr <108723529+sirine-Tr@users.noreply.github.com>
2023-01-28 15:03:29 +0100

    placement of a cross

--- AR Meuble/Assets/Scripts/InputManager.cs
+++ AR Meuble/Assets/Scripts/InputManager.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     //referance to the object (sofa)
     //[SerializeField] private GameObject arObj;
     //camera
+    //[SerializeField] could change the value from editeur
     [SerializeField] private Camera arCam;
     [SerializeField] private ARRaycastManager _raycastManager;
     private List <ARRaycastHit> _hits = new List <ARRaycastHit>();
+    //get the tache of the user in the screen
+    private Touch touch;
+    private Pose pose;
+    //to cross the space 
+    [SerializeField] private GameObject crosshair;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +26,43 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CrosshairCalculation();
+        // when the user click on the screen the object apear
+        touch = Input.GetTouch(0);
+        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began)
+             return;
+
+        if (IsPointerOverUI (touch)) return;     
+        Instantiate (DataHandler.Instance.furniture, pose.position, pose.rotation);
+            
+
+        
+    }
+
+    bool IsPointerOverUI (Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData (EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List <RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+
+    //calculating the space 
+    void CrosshairCalculation()
+    {
+        //point in the center of the screen
+        Vector3 origin = arCam.ViewportToScreenPoint( new Vector3(0.5f, 0.5f, 0));
         //when the user tab the camera read the location of mouseposition
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+       
+            Ray ray = arCam.ScreenPointToRay(origin);
             if(_raycastManager.Raycast(ray, _hits))
             {
                 //pose give the position and the rotation of arObj
-                Pose pose = _hits[0].pose;
-                Instantiate (DataHandler.Instance.furniture, pose.position, pose.rotation);
+                 pose = _hits[0].pose;
+                crosshair.transform.position = pose.position;
+                crosshair.transform.eulerAngles = new Vector3(90,0,0);
             }
-            
 
-        }
     }
 }
