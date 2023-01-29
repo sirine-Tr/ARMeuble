72c4dcb26e7fc977d4a3330097297f6ef314cd16
sirine-Tr <108723529+sirine-Tr@users.noreply.github.com>
2023-01-29 10:47:07 +0100

    local forniture

--- AR Meuble/Assets/Scripts/DataHandler.cs
+++ AR Meuble/Assets/Scripts/DataHandler.cs
@@ -6,12 +6,12 @@ public class DataHandler : MonoBehaviour
 {
     public GameObject furniture;
     //get information from button 
-    [SerializeField] private ButtonManager buttonManager;
+    [SerializeField] private ButtonManager buttonPrefab;
     // the content of the button
     [SerializeField] private GameObject buttonContainter;
     //save all the items
-    [SerializeField] private List <Item> items;
-    [SerializeField] private ButtonManager buttonPrefab;
+    [SerializeField] private List <Item> _items;
+    //[SerializeField] private ButtonManager buttonPrefab;
     private int current_id = 0;
     private static DataHandler instance;
     //methode to create an instance
@@ -26,36 +26,36 @@ public class DataHandler : MonoBehaviour
             return instance;
         }
     }
-private void Start()
-{
-    LoadItems();
-    CreateButtons();
-}
+    private void Start()
+      {
+         LoadItems();
+         CreateButtons();
+      }
     void LoadItems()
     {
-        var items_obj = Resources.LoadAll("Items", typeof(Item));
-        foreach (var item in items_obj)
-        {
-            items.Add((Item)item);
-        }
+        var items_obj =Resources.LoadAll("Items",typeof(Item));
+       foreach (var item in items_obj)
+       {
+           _items.Add(item as Item);
+       }
     }
     //responsable to render the button dynamique
     void CreateButtons()
     {
-        foreach (Item i in items)
+        foreach (Item i in _items)
         {
             ButtonManager b = Instantiate(buttonPrefab, buttonContainter.transform);
             b.ItemId = current_id;
             b.ButtonTexture = i.itemImage;
-            current_id ++;
-
+            current_id++;
         }
+        //buttonContainer.GetComponent<UIContentFitter>().Fit();
         
 
     }
     public void SetFurniture (int id)
         {
-            furniture = items[id].itemPrefab;
+            furniture = _items[id].itemPrefab;
 
 
         }
